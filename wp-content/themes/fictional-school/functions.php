53a3f6f15ce26de520c71a37f147f27fa617ed3f
Carlos Blandino <cablandino66@gmail.com>
2020-07-22 23:11:54 -0700

    adding child pages Our Goals, Our History, and Cookie Policy, converting hard coded properties to dynamic

--- wp-content/themes/fictional-school/functions.php
+++ wp-content/themes/fictional-school/functions.php
@@ -9,3 +9,8 @@
         wp_enqueue_style('schoolMainStyles',get_stylesheet_uri());
     }
     add_action('wp_enqueue_scripts','schoolFiles');
+
+    function universityFeature() {
+        add_theme_support('title-tag');
+    }
+    add_action('after_setup_theme', 'universityFeature');
